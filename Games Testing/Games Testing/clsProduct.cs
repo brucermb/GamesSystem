476b996a4c3571ad8d888f59cc7fa21675872999
Bruce Bradshaw <p1715872x@my365.dmu.ac.uk>
2020-02-07 14:10:41 +0000

    Added testing and class for address management

---
+++ Games Testing/Games Testing/clsProduct.cs
@@ -0,0 +1,6 @@
+﻿namespace Games_Testing
+{
+    internal class clsProduct
+    {
+    }
+}
---
+++ Games Testing/Games Testing/tstProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Games_Testing
+{
+    [TestClass]
+    public class tstProduct
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class we want to create
+            clsProduct AProduct = new clsProduct();
+            //test to see that it exists
+            Assert.IsNotNull(AProduct);
+        }
+        
+        }
+    }
