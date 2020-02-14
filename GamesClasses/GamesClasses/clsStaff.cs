759ae3728b1a38ef72d0b8b2ccb7e6edc060222c
adamrichardson11@yahoo.co.uk <adamrichardson11@yahoo.co.uk>
2020-02-14 14:54:58 +0000

    added tests for the name, hire date and active properties

---
+++ GamesClasses/GamesClasses/clsStaff.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GamesClasses
+{
+    public class clsStaff
+    {
+        public bool Active { get; set; }
+        public DateTime DateHired { get; set; }
+        public string Name { get; set; }
+    }
+}
