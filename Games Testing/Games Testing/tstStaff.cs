9d580b14f8d31a713a52fb82f826d4faae624875
adamrichardson11@yahoo.co.uk <adamrichardson11@yahoo.co.uk>
2020-02-21 14:59:56 +0000

    added more tests and presentation layer

--- Games Testing/Games Testing/tstStaff.cs
+++ Games Testing/Games Testing/tstStaff.cs
@@ -28,6 +28,18 @@ namespace Games_Testing
             // test to see if the two values are the same
             Assert.AreEqual(AStaff.Active, TestData);
         }
+        [TestMethod]
+        public void AccountIdPropertyOk()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            int TestData = 1;
+            // assign the data to the property
+            AStaff.AccountId = TestData;
+            // test to see if the two values are the same
+            Assert.AreEqual(AStaff.AccountId, TestData);
+        }
 
         [TestMethod]
         public void HireDatePropertyOk()
@@ -52,7 +64,55 @@ namespace Games_Testing
             AStaff.Name = TestData;
             // test to see if the two values are the same
             Assert.AreEqual(AStaff.Name, TestData);
-        } 
+        }
+        [TestMethod]
+        public void DateOfBirthPropertyOk()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            // assign the data to the property
+            AStaff.DOB = TestData;
+            // test to see if the two values are the same
+            Assert.AreEqual(AStaff.DOB, TestData);
+        }
+        [TestMethod]
+        public void UsernamePropertyOk()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            string TestData = "GavinoFree";
+            // assign the data to the property
+            AStaff.Username = TestData;
+            // test to see if the two values are the same
+            Assert.AreEqual(AStaff.Username, TestData);
+        }
+        [TestMethod]
+        public void PasswordPropertyOk()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            string TestData = "password";
+            // assign the data to the property
+            AStaff.Password = TestData;
+            // test to see if the two values are the same
+            Assert.AreEqual(AStaff.Password, TestData);
+        }
+        [TestMethod]
+        public void StaffAdminPermissionPropertyOk()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            Boolean TestData = true;
+            // assign the data to the property
+            AStaff.StaffAdmin = TestData;
+            // test to see if the two values are the same
+            Assert.AreEqual(AStaff.StaffAdmin, TestData);
+        }
 
 
     }
