ab9fef4aa63d78e519fffd366625e98947238abd
Mr Rana <tejrana@hotmail.com>
2020-05-18 02:37:59 +0100

    I have added the add, find and detete method

--- Games Testing/Games Testing/TestCustomer.cs
+++ Games Testing/Games Testing/TestCustomer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using GamesClasses;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_Library;
 
 namespace Games_Testing
 {
@@ -30,12 +31,12 @@ namespace Games_Testing
         }
 
         [TestMethod]
-        public void CustomerDateOK()
+        public void CustomerDateAddedOK()
         {
             clsCustomer.anCustomer = new clsCustomer();
-                DateTime TestData = DateTime.Now.Date;
-                 anCustomer.CustomerDateAdded = TestData;
-                     Assert.AreEqual(anCustomer.CustomerDateAdded, TestData);
+            DateTime TestData = DateTime.Now.Date;
+            anCustomer.CustomerDateAdded = TestData;
+            Assert.AreEqual(anCustomer.CustomerDateAdded, TestData);
         }
 
         [TestMethod]
@@ -94,12 +95,12 @@ namespace Games_Testing
         }
 
         [TestMethod]
-        public void CustomerCityOK()
+        public void CustomerTownOK()
         {
             clsCustomer anCustomer = new clsCustomer();
             String TestData = "London";
-            anCustomer.CustomerCity = TestData;
-            Assert.AreEqual(anCustomer.CustomerCity, TestData);
+            anCustomer.CustomerTown = TestData;
+            Assert.AreEqual(anCustomer.CustomerTown, TestData);
         }
 
         [TestMethod]
@@ -153,12 +154,24 @@ namespace Games_Testing
             Boolean OK = true;
             Int32 CustomerNo = 1;
             Found = anCustomer.Find(CustomerNo);
-            if (anCustomer.CustomerDateAdded != Convert.ToDateTime("1/10/2018"))
+            if (anCustomer.CustomerDateAdded != Convert.ToDateTime("01/10/2018"))
             {
                 OK = false;
             }
 
             Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void DateOfBirthPropertyOk()
+        {
+            // create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            // create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            // assign the data to the property
+            ACustomer.DOB = TestData;
+            // test to see if the two values are the same
+            Assert.AreEqual(ACustomer.DOB, TestData);
         }
 
         [TestMethod]
@@ -196,11 +209,12 @@ namespace Games_Testing
             // create an instance of the class we want to create
             clsCustomer ACustomer = new clsCustomer();
             // create some test data to assign to the property
-            string TestData = "GavinoFree";
+            string TestData = "MrRana";
             // assign the data to the property
             ACustomer.Username = TestData;
             // test to see if the two values are the same
             Assert.AreEqual(ACustomer.Username, TestData);
+
         }
 
         [TestMethod]
@@ -220,7 +234,6 @@ namespace Games_Testing
         [TestMethod]
         public void PasswordPropertyOk()
         {
-            // create an instance of the class we want to create
             clsCustomer ACustomer = new clsCustomer();
             // create some test data to assign to the property
             string TestData = "password";
@@ -229,13 +242,135 @@ namespace Games_Testing
             // test to see if the two values are the same
             Assert.AreEqual(ACustomer.Password, TestData);
         }
-        
+
+{
+        [TestClass]
+
+        public class tstAddress
+        {
+            //good test data
+            //create some test data to pass to the method
+            string CustomerAddress = "10 Downing Street";
+            string CustomerEmail = "tejrana@hotmail.com";
+            String Customertown = "London";
+            string CustomerDateAdded = "01/10/2018";
+            string CutsomerPostcode = "SW1A 2AA";
+        }
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to creatre
+            clsCustomerAddress AnCustomerAddress = new clsCustomerAddress();
+            //boolean variable to store the results of the validation
+            Boolean found = false;
+            //create some test data to use with the method
+            Int32 CustomerAdressNo = 1;
+            //invoke the method
+            found = CustomerAddress.Find(CustomerAddressOK);
+            //test to see that the result is correct
+            Assert.IsTrue(found);
         }
 
+        [TestMethod]
+        public void CustomerAddressNoFound()
+        {
+            //create an instance of the class we want to creatre
+            clsCustomerAddress AnCustomerAddress = new clsCustomerAddress();
+            //boolean variable to store the results of the validation
+            Boolean found = false;
+            //create some test data to use with the method
+            Int32 CustomerAddressNo = 21;
+            //invoke the method
+            found = CustomerAddress.Find(CustomerAddressOK);
+            //check the Customers Address No
+            if (AnCustomerAddress.CustomerAddressNo != 22)
+            {
+                OK = false;
+            }
+            //test to see if result is correct
+            Assert.IsTrue(OK);
+        }
 
-    }
-Assert.IsTrue(OK);
-                }
-           }
-    }
-   
+        [TestMethod]
+        public void
+            public void addMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerAddressCollection AllCustomerAddress = new clsCustomerAddressCollection();
+            //create the item of test data
+            clsCustomerAddress TestItem = new clsCustomerAddress();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.Active = true;
+            TestItem.CustomerNo = 1
+                TestItem.CustomerAddressNo = 10;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.HouseNo = "10 Dowing Street"
+            TestItem.Postcode = "SW1A 2AA";
+            TestItem.Town = "London";
+            //set CustomerAddress to the test data
+            AllCustomerAddress.CustomerAddress = TestItem
+                //add the record
+                PrimaryKey = CustomerAddress.add();
+            //set the primary key of the test data
+            TestItem.CustomerNo = PrimaryKey;
+            //find the record
+            AllCustomerAddress.CustomerAddress.Find(PrimaryKey);
+            //delete the record
+            AllCustomerAddress.delete();
+            //now find the record
+            Boolean Found = AllCustomerAddress.CustomerAddress.Fidn(PrimaryKey);
+            //test to see that the record was not found
+            Assert.AreEqual(AllCustomerAddress.ThisCustomerAddress, TestItem);
+
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerAddressCollection AllCustomerAddress = new clsCustomerAddressCollection();
+            //create the item of test data
+            clsCustomerAddress TestItem = new clsCustomerAddress();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.Active = true;
+            TestItem.CustomerNo = 1
+                TestItem.CustomerAddressNo = 10;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.HouseNo = "10 Dowing Street"
+            TestItem.Postcode = "SW1A 2AA";
+            TestItem.Town = "London";
+            //set CustomerAddress to the test data
+            AllCustomerAddress.CustomerAddress = TestItem
+                //add the record
+           int PrimaryKey = CustomerAddress.add();
+            //set the primary key of the test data
+            TestItem.CustomerNo = PrimaryKey;
+            //find the record
+            AllCustomerAddress.CustomerAddress.Find(PrimaryKey);
+            //delete the record
+            AllCustomerAddress.delete();
+            //now find the record
+            Boolean Found = AllCustomerAddress.CustomerAddress.Fidn(PrimaryKey);
+            //test to see that the record was not found
+            Assert.IsFalse(Found);
+        }
+
+        [TestMethod]
+        public void ReportByPostCodeMethodOK()
+        {
+            //create an instance of the class containing unfiltered results
+            clsCustomerAddressCollection AllCustomerAddress = new clsCustomerAddressCollection();
+            //create an instance of the filtered data
+            clsCustomerAddressCollection FilteredCustomerAddresses = new clsCustomerAddressCollection();
+            //apply a blank string (should return all records);
+            FilteredCustomerAddresses.ReportByPostCose("");
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCustomerAddress.count, FilteredCustomerAddresses.count);
+        } }
+
+        }
+          
+                
+         
--- GamesClasses/GamesClasses/clsCustomer.cs
+++ GamesClasses/GamesClasses/clsCustomer.cs
@@ -1,129 +1,240 @@
 ﻿using System;
+using System.Web.Configuration;
+
+private decimal mCustomerTotal;
 
 namespace GamesClasses
 {
-     private Boolean mCustomerActive;
-    private Int1 mCustomerNo;
-    private DateTime mCustomerDateAdded;
-    private decimal mCustomerTotal;
-    private string mCustomerLastName;
-    private string mCustomerFirstName;
-    private string mCustomerAddress;
-    private string mCustomerPostcode;
-    private string mCustomerCity;
-    private string mCustomerPhoneNumber;
-    private string mCustomerEmail;
-
-    public int CustomerNo
-    {
-        get => mCustomerNo;
-
-        set => mCustomerNo = value;
-    }
-
-    public Boolean CustomerActive
-    {
-        get => mCustomerActive;
-        set => mCustomerActive = value;
-    }
-    public System.DateTime CustomerDateAdded
-    {
-        get => mCustomerDateAdded;
-        set => mCustomerDateAdded = value;
-    }
-
-    public decimal CustomerTotal
-    {
-        get => mCustomerTotal;
-        set => mCustomerTotal = value;
-    }
-    public string CustomerFirstName
-    {
-        get => mCustomerFirstName;
-        set => mCustomerFirstName = value;
-    }
-    public string CustomerLastName
-    {
-        get => mCustomerLastName;
-        set => mCustomerLastName = value;
-+   }
-    public string CustomerAddress
-    {
-        get => mCustomerAddress;
-        set => mCustomerAddress = value;
-    }
-    public string CustomerPostcode
-    {
-        get => mCustomerPostcode;
-        set => mCustomerPostcode = value;
-    }
-    public string CustomerCity
-    {
-        get => mCustomerCity;
-        set => mCustomerCity = value;
-    }
-    public string CustomerPhoneNumber
-    {
-        get => mCustomerPhoneNumber;
-        set => mCustomerPhoneNumber = value;
-    }
-    public string CustomerEmail
-    {
-        get => mCustomerEmail;
-        set => mCustomerEmail = value;
-    }
-
-    public object Convert { get; private set; }
-
-    public bool Find(int CustomerNo)
-    {
-        //create an instance of the data connection 
-        clsDataConnection DB = new clsDataConnection();
-        //add the parameter for the Customer No to search for
-        DB.AddParameter("@CustomerNo", CustomerNo);
-        //execute the stored procedure 
-        DB.Execute("sproc_tblCustomer_FilterByCustomerNo");
-
-        //If one record is found (there should be either one or zero)
-        if (DB.Count == 1)
+        public class clsCustomer
         {
-            mCustomerActive = Convert.ToBoolean(DB.DataTable.Rows[0]["CustomerActive"]);
-            mCustomerFirstName = Convert.ToString(DB.DataTable.Rows[0]["CustomerFirstName"]);
-            mCustomerLastName = Convert.ToString(DB.DataTable.Rows[0]["CustomerLastName"]);
-            mCustomerAddress = Convert.ToString(DB.DataTable.Rows[0]["CustomerAddress"]);
-            mCustomerPostcode = Convert.ToString(DB.DataTable.Rows[0]["CustomerPostcode"]);
-            mCustomerCity = Convert.ToString(DB.DataTable.Rows[0]["CustomerCity"]);
-            mCustomerPhoneNumber = Convert.ToString(DB.DataTable.Rows[0]["CustomerPhoneNumber"]);
-            mCustomerEmail = Convert.ToString(DB.DataTable.Rows[0]["CustomerEmail"]);
-            mCustomerTotal = Convert.ToDecimal(DB.DataTable.Rows[0]["CustomerTotal"]);
-            mCustomerDateAdded = Convert.ToDateTime(DB.DataTable.Rows[0]["CustomerDateAdded"]);
-            mCustomerNo = Convert.ToInt1(DB.DataTable.Rows[0]["CustomerNo"]);
-            //return that everything worked OK
-            return true;
-        }
-        //if no record was found
-        else
-        {
-            //return false indicating a problem
-            return false;
-        }
-    }
+            private Boolean mCustomerActive;
+            private int mCustomerNo;
+            private DateTime mCustomerDateAdded;
+            private decimal mCustomerTotal;
+            private string mCustomerFirstName;
+            private string mCustomerLastName;
+            private string mCustomerAddress;
+            private string mCustomerPostcode;
+            private string mCustomerTown;
+            private string mCustomerPhoneNumber;
+            private string mCustomerEmail;
+            private string mCutomerDateOfBirth;
+        private string mCustomerDateOfBirth;
 
-    public void Delete(Int1 CustomerNo)
+        public Boolean CustomerActive
+            {
+                get
+                {
+                    return mCustomerActive;
+                }
+                set
+                {
+                    mCustomerActive = value;
+                }
+            }
+            public DateTime CustomerDateAdded
+            {
+                get
+                {
+                    return mCustomerDateAdded;
+                }
+                set
+                {
+                    mCustomerDateAdded = value;
+                }
+            }
+            public int CustomerNo
+            {
+                get
+                {
+                    return mCustomerNo;
+                }
+                set
+                {
+                    mCustomerNo = value;
+                }
+            }
+            public decimal CustomerTotal
+            {
+                get
+                {
+                    return mCustomerTotal;
+                }
+                set
+                {
+                    mCustomerTotal = value;
+                }
+            }
+            public string CustomerLastName
+            {
+                get
+                {
+                    return mCustomerLastName;
+                }
+                set
+                {
+                    mCustomerLastName = value;
+                }
+            }
+
+            public string CustomerFirstName
+            {
+                get
+                {
+                    return mCustomerFirstName;
+                }
+                set
+                {
+                    mCustomerFirstName = value;
+                }
+            }
+            
+            public string CustomerAddress
+            {
+                get
+                {
+                    return mCustomerAddress;
+                }
+                set
+                {
+                    mCustomerAddress = value;
+                }
+            }
+            public string CustomerPostcode
+            {
+                get
+                {
+                    return mCustomerPostcode;
+                }
+                set
+                {
+                    mCustomerPostcode = value;
+                }
+            }
+            public string CustomerTown
+            {
+                get
+                {
+                    return mCustomerTown;
+                }
+                set
+                {
+                    mCustomerTown = value;
+                }
+            }
+            public string CustomerPhoneNumber
+            {
+                get
+                {
+                    return mCustomerPhoneNumber;
+                }
+                set
+                {
+                    mCustomerPhoneNumber = value;
+                }
+            }
+            public string CustomerEmail
+            {
+                get
+                {
+                    return mCustomerEmail;
+                }
+                set
+                {
+                    mCustomerEmail = value;
+                }
+                public string CustomerDateOfBirth
+            {
+                get
+                {
+                    return mCustomerDateOfBirth;
+                }
+                set
+                {
+                    mCustomerDateOfBirth = value;
+                }
+                }
+            }
+            }
+
+            public interface add()
     {
-        //create an instance of the data connection 
-        clsDataConnection DB = new clsDataConnection();
-        //add the parameter for the Customer No to search for
-        DB.AddParameter("@CustomerNo", CustomerNo);
-        //execute the stored procedure 
-        DB.Execute("sproc_tblCustomer_Delete");
+    //adds a new record to the database based on the values of the MCustomerAddress
+    //set the primary key value of the new record
+    mCustomerAddress.CustomerAddressNo = 12
+    //return the primary key of the new record
+    return mCustomerAddress.CustomerAddress
     }
+            public bool Find(int CustomerAddress)
+            {
+                //create an instance of the data connection 
+                clsDataConnection DB = new clsDataConnection();
+    //add the parameter for the Customer No to search for
+    DB.AddParameter("@CustomerAddress", CustomerAddress);
+    //execute the stored procedure 
+    DB.Execute("sproc_tblCustomer_FilterByCustomerAddress");
+
+                //If one record is found (there should be either one or zero)
+                if (DB.Count == 1)
+                {
+        mCustomerAddress = Convert.ToString(DB.DataTable.Rows[0]["CustomerAddress"]);
+                    mCustomerNo = Convert.ToInt32(DB.DataTable.Rows[0]["CustomerNo"]);
+                    mCustomerActive = Convert.ToBoolean(DB.DataTable.Rows[0]["CustomerActive"]);
+                    mCustomerLastName = Convert.ToString(DB.DataTable.Rows[0]["CustomerLastName"]);
+                    mCustomerFirstName = Convert.ToString(DB.DataTable.Rows[0]["CustomerFirstName"]);
+                    mCustomerPostcode = Convert.ToString(DB.DataTable.Rows[0]["CustomerPostcode"]);
+                    mCustomerTown = Convert.ToString(DB.DataTable.Rows[0]["CustomerTown"]);
+                    mCustomerPhoneNumber = Convert.ToString(DB.DataTable.Rows[0]["CustomerPhoneNumber"]);
+                    mCustomerEmail = Convert.ToString(DB.DataTable.Rows[0]["CustomerEmail"]);
+                    mCustomerTotal = Convert.ToDecimal(DB.DataTable.Rows[0]["CustomerTotal"]);
+                    mCustomerDateAdded = Convert.ToDateTime(DB.DataTable.Rows[0]["CustomerDateAdded"]);
+                    mCustomerDateOfBirth = Convert.ToDateTime(DB.DataTable.Rows[0]["CustomerDateOfBirthAdded"]);
+                    
+                    //return that everything worked OK
+                    return true;
+                }
+                //if no record was found
+                else
+                {
+                    //return false indicating a problem
+                    return false;
+                }
+            }
+
+            public void Delete(int CustomerNo)
+            {
+                //create an instance of the data connection 
+                clsDataConnection DB = new clsDataConnection();
+                //add the parameter for the Customer No to search for
+                DB.AddParameter("@CustomerNo", CustomerNo);
+                //execute the stored procedure 
+                DB.Execute("sproc_tblCustomer_Delete");
+            }
 
 
 
-}
-
-public class Int1
+        }
+    }
+    public bool Find(int CustomerAddress)
 {
+    //set the private data members to the test data value
+    CustomerAddress = 10 Downing Street;
+    //create an instance of the data connection 
+    clsDataConnection DB = new clsDataConnection();
+    //add the parameter for the Customer No to search for
+    DB.AddParameter("@CustomerAddress", CustomerAddress
+    //execute the stored procedure 
+    DB.Execute("sproc_tblCustomer_FilterByCustomerNo");
 }
+
+public void Delete()
+{
+    //deletes the record pointed to by CustomerAddress
+    //connect to the database
+    clsDataConnection DB = new clsDataConnection();
+    //set the parameters for the stored procedure
+    DB.AddParameter("@CustomerAddress", mCustomerAddress);
+    //execute the stored procedure
+    DB.Execute("sproc_tblCustomerAddress_Delete");
 }
+        
